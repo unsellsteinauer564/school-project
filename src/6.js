c95c58088d27af66a562b469b37fbe3f7ffd7012
unsellsteinauer564 <unsellsteinauer564@gmail.com>
2025-03-11 18:52:46 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,8 @@
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const result = getRandomNumber(1, 10);
+console.log(result);
